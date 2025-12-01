60e6364b206f3aa0dab507c483281d77387775f6
scain-ui <scain@midland.k12.ia.us>
2025-12-01 18:11:36 +0000

    1

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 function markAsDone(todos) {
-    return transform(0, todos, []);
-}
-
-function transform(index, todos, modifiedTodos){
-    if(index < todos.length){
-        // on this line - prepend the string 'done - ' to each `todo` description
-        // on this line - call transform recursively
-        return modifiedTodos
-    }else{
-        return modifiedTodos
+    for (let i = 0; i < todos.length; i++) {
+        todos[i] = 'done - ' + todos[i]
     }
+    return todos;
 }
